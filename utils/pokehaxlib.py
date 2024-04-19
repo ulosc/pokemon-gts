b6450ab731ed3c3e599a36cf46811ef2a167f83a
ulosc <53043999+ulosc@users.noreply.github.com>
2024-04-19 23:52:31 +0900

    Refactored response class

--- utils/pokehaxlib.py
+++ utils/pokehaxlib.py
@@ -1,3 +1,4 @@
+import datetime as dt
 import socket
 import hashlib
 import time
@@ -38,61 +39,35 @@ class Request:
 
 
 class Response:
-    def __init__(self, h):
-        self.is_pkm_data = False
-        if isinstance(h, bytes):
-            try:
-                h = h.decode()
-            except UnicodeDecodeError:
-                self.is_pkm_data = True
-                self.data = h
-                return
-        if not h.startswith("HTTP/1.1"):
-            self.data=h
-            return
-        h=h.split("\r\n")
-        while True:
-            line=h.pop(0)
-            if not line:
-                break
-            elif line.startswith("P3P"):
-                self.p3p=line[line.find(": ")+2:] # unknown
-            elif line.startswith("cluster-server"):
-                self.server=line[line.find(": ")+2:] # unknown
-            elif line.startswith("X-Server-"):
-                self.sname=line[line.find(": ")+2:] # unknown
-            elif line.startswith("Content-Length"):
-                self.len=int(line[line.find(": ")+2:]) # necessary
-            elif line.startswith("Set-Cookie"):
-                self.cookie=line[line.find(": ")+2:] # unnecessary
-        self.data="\r\n".join(h)
-
-    def get_bytes(self):
-        months=[
-            "???", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct",
-            "Nov", "Dec"
-        ]
-        days=["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
-        t=time.gmtime()
-        str_repr = (
-            "HTTP/1.1 200 OK\r\n" +
-            "Date: %s, %02i %s %i %02i:%02i:%02i GMT\r\n"%(
-                    days[t[6]], t[2], months[t[1]], t[0], t[3], t[4], t[5]
-            ) +
-            "Server: Microsoft-IIS/6.0\r\n" +
-            "P3P: CP='NOI ADMa OUR STP'\r\n" +
-            "cluster-server: aphexweb3\r\n" +
-            "X-Server-Name: AW4\r\n" +
-            "X-Powered-By: ASP.NET\r\n" +
-            "Content-Length: %i\r\n"%len(self.data) +
-            "Content-Type: text/html\r\n" +
-            "Set-Cookie: ASPSESSIONIDQCDBDDQS=JFDOAMPAGACBDMLNLFBCCNCI; path=/\r\n" +
-            "Cache-control: private\r\n\r\n"
-        )
-        if self.is_pkm_data:
-            return str_repr.encode() + self.data
+    def __init__(self, data: str | bytes) -> None:
+        if isinstance(data, str):
+            data = data.encode()
+        elif isinstance(data, bytes):
+            pass
         else:
-            return str_repr.encode() + self.data.encode()
+            raise TypeError(f'Expected str or bytes but got {type(data)}')
+        self.data = data
+
+    def __bytes__(self) -> bytes:
+        now = dt.datetime.now(dt.timezone.utc).strftime('Date: %a, %d %b %Y %H:%M:%S GMT\r\n')
+        content_length = f"Content-Length: {len(self)}\r\n"
+        return (
+            b"HTTP/1.1 200 OK\r\n" +
+            now.encode() +
+            b"Server: Microsoft-IIS/6.0\r\n" +
+            b"P3P: CP='NOI ADMa OUR STP'\r\n" +
+            b"cluster-server: aphexweb3\r\n" +
+            b"X-Server-Name: AW4\r\n" +
+            b"X-Powered-By: ASP.NET\r\n" +
+            content_length.encode() +
+            b"Content-Type: text/html\r\n" +
+            b"Set-Cookie: ASPSESSIONIDQCDBDDQS=JFDOAMPAGACBDMLNLFBCCNCI; path=/\r\n" +
+            b"Cache-control: private\r\n\r\n" +
+            self.data
+        )
+
+    def __len__(self) -> int:
+        return len(self.data)
 
 
 def encode_pkm(pkm_path: Path) -> tuple[bytes, bytes]:
@@ -110,7 +85,7 @@ def encode_pkm(pkm_path: Path) -> tuple[bytes, bytes]:
         encoded_pkm += (pkm[0x40] & 2 + 1).to_bytes()
     encoded_pkm += pkm[0x8c:0x8c+1]  # level
     # request フシギダネ with either gender at any level
-    encoded_pkm += b'\x01\x00\x03\x00\x00\x00\x00\x00'
+    encoded_pkm += b'\x01\x00\x03\x00\x00\x00\x00\x00'  # request
     encoded_pkm += b'\xdb\x07\x03\x0a\x00\x00\x00\x00'  # date deposited as 3/10/2011
     encoded_pkm += b'\xdb\x07\x03\x16\x01\x30\x00\x00'  # date traded
     encoded_pkm += pkm[0x00:0x04]  # PID
@@ -119,7 +94,7 @@ def encode_pkm(pkm_path: Path) -> tuple[bytes, bytes]:
     encoded_pkm += pkm[0x68:0x78]  # original trainer Name
     encoded_pkm += b'\xDB\x02'  # country, city
     encoded_pkm += b'\x46\x01\x15\x02'  # sprite, exchanged, version, language
-    encoded_pkm += b'\x01\x00'
+    encoded_pkm += b'\x01\x00'  # unknown
     return pkm, encoded_pkm
 
 
@@ -149,7 +124,7 @@ def encode_response(data: bytes, encoded_pkm: bytes) -> tuple[bytes, bool]:
             gts.salt.encode() + urlsafe_b64encode(response) + gts.salt.encode()
         )
         response += m.hexdigest().encode()
-    response = Response(response).get_bytes()
+    response = bytes(Response(response))
     return response, is_sent
 
 
